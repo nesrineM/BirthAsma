612d95445943d67f9feca5cbda6e2c19b452c38d
Nesrine <nesrineM@users.noreply.github.com>
2025-11-26 15:23:01 +0100

    Add files via upload

---
+++ heart_lines.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(6,6))
+plt.axis("equal")
+plt.axis("off")
+
+# Heart equation
+t = np.linspace(0, 2*np.pi, 400)
+x = 16 * np.sin(t)**3
+y = (13 * np.cos(t)) - (5 * np.cos(2*t)) - (2 * np.cos(3*t)) - (np.cos(4*t))
+
+# Draw radiating lines
+for i in range(len(x)):
+    plt.plot([0, x[i]], [0, y[i]], linewidth=0.4)
+
+plt.show()
